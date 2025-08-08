87e37fd2e5e1456d79dbfe10044bf94c08722ad8
Andrew sooter <andrewsooter121@gmail.com>
2025-08-09 04:55:46 +0530

    Add sample env.

--- handler/root.go
+++ handler/root.go
@@ -1,14 +1,9 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type User struct {
-}
-
 func (app *Application) HandleRootRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("handle root request")
 
 }
